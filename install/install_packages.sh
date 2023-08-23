db04890c40e53f8fef91db8613aef08f777c43cb
kevin-eschbach <64735549+kevin-eschbach@users.noreply.github.com>
2023-08-23 20:56:15 +0200

    install miniconda

--- install/install_packages.sh
+++ install/install_packages.sh
@@ -44,4 +44,9 @@ else
 	echo "build-essential failed to install" >> $log_file
 fi
 
+mkdir ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
 curl -fsSl https://fnm.vercel.app/install | bash
