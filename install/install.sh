a1a62310a193049a510c313eb80e804c5f928607
Kevin Eschbach <64735549+kevin-eschbach@users.noreply.github.com>
2023-12-19 15:53:35 +0100

    finishing touches

--- install/install.sh
+++ install/install.sh
@@ -10,6 +10,9 @@ sudo rm -rf ~/.bash_profile > /dev/null 2>&1
 sudo rm -rf ~/.gitconfig > /dev/null 2>&1
 sudo rm -rf ~/.config/nvim > /dev/null 2>&1
 sudo rm -rf ~/.newsboat > /dev/null 2>&1
+sudo rm -rf ~/.config/i3 > /dev/null 2>&1
+sudo rm -rf ~/.config/i3status > /dev/null 2>&1
+sudo rm -f ~/.config/compton.conf > /dev/null 2>&1
 
 ## Create symlinks
 echo "Creating symlinks..."
@@ -17,3 +20,6 @@ ln -sf $dotfiles_dir/bashrc ~/.bashrc
 ln -sf $dotfiles_dir/nvim ~/.config/nvim
 ln -sf $dotfiles_dir/newsboat ~/.newsboat
 ln -sf $dotfiles_dir/git/.gitconfig ~/.gitconfig
+ln -sf $dotfiles_dir/i3 ~/.config/i3
+ln -sf $dotfiles_dir/i3status ~/.config/i3status
+ln -sf $dotfiles_dir/compton/compton.conf ~/.config/compton.conf
