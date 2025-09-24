603012c0211e6de75459c65a336230a9928500f5
Kevin Eschbach <64735549+kevin-eschbach@users.noreply.github.com>
2025-09-24 02:54:52 +0200

    feat(polybar): mirror laptopscreen on startup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x -u $UID polybar >/dev/null; do sleep 1; done
 
+# Let all monitors mirror the laptop
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      xrandr --output $m --same-as eDP-1
+  done
+fi
+
 # Launch bar
-MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-MONITORS=$MONITORS polybar example
-
-echo "polybar started..."
+polybar --reload example
