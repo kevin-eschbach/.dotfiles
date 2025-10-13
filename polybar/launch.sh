33a1d60842df84ac22028f49ace161fafdeb5ab0
kevin-eschbach <64735549+kevin-eschbach@users.noreply.github.com>
2025-10-14 01:28:17 +0200

    feat(i3): better monitor managing

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x -u $UID polybar >/dev/null; do sleep 1; done
 
-# Let all monitors mirror the laptop
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      xrandr --output $m --same-as eDP-1
-  done
+if command -v xrandr >/dev/null; then
+    # Get all connected outputs except eDP-1
+    mapfile -t outputs < <(xrandr --query | grep " connected" | cut -d" " -f1 | grep -v "eDP-1")
+    if [ "${#outputs[@]}" -gt 0 ]; then
+        # Turn on all external, turn off eDP-1
+        for m in "${outputs[@]}"; do
+            xrandr --output "$m" --auto
+        done
+        xrandr --output "eDP-1" --off
+    else
+        # Only eDP-1 is connected, turn it on
+        xrandr --output "eDP-1" --auto
+    fi
 fi
 
 # Launch bar
